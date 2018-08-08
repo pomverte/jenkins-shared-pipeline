8cdb170f33fcc2cc13386bfca268d703b6baca21
pomverte <vietnem@yahoo.fr>
2018-08-08 21:18:17 +0200

    display information

--- vars/myDeliveryPipeline.groovy
+++ vars/myDeliveryPipeline.groovy
@@ -38,23 +38,7 @@ def call(Closure body) {
 
       stage('Information') {
         steps {
-          script {
-            gitCommitId = sh(returnStdout: true, script: 'git rev-parse --short HEAD').trim()
-            def author = sh(returnStdout: true, script: 'git --no-pager show -s --format=\'%an\' HEAD').trim()
-            def authorEmail = sh(returnStdout: true, script: 'git --no-pager show -s --format=\'%ae\' HEAD').trim()
-            def comment = sh(returnStdout: true, script: 'git --no-pager show -s --format=\'%B\' HEAD').trim()
-            echo """
-
-Branch : ${env.BRANCH_NAME}
-Author : ${author}
-Email : ${authorEmail}
-Commit : ${gitCommitId}
-Comment : ${comment}
-ArtifactId : ${ARTIFACT_ID}
-Version : ${ARTIFACT_VERSION}
-
-          """
-          }
+          displayInformation
         }
       }
 
