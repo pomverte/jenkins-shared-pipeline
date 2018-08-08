ab70698cba9ac21987c31c2314576350969ebcab
pomverte <vietnem@yahoo.fr>
2018-08-09 01:03:43 +0200

    docker pipeline withCredentials

--- vars/dockerPipeline.groovy
+++ vars/dockerPipeline.groovy
@@ -57,12 +57,21 @@ def call(Closure body) {
       }
       stage('Docker image push') {
         steps {
-          echo 'TODO docker image push ...'
-          //sh 'docker login -u=${DOCKER_REGISTRY_USER} -p=${DOCKER_REGISTRY_PASSWORD} ${DOCKER_REGISTRY_SERVER}'
-          //sh 'docker image push ${DOCKER_REGISTRY_USER}/${ARTIFACT_ID}:${ARTIFACT_VERSION}'
-          //sh 'docker image push ${DOCKER_REGISTRY_USER}/${ARTIFACT_ID}:${gitCommitId}'
-          //sh 'docker image push ${DOCKER_REGISTRY_USER}/${ARTIFACT_ID}:latest'
-          //sh 'docker logout'
+          script {
+            withCredentials([usernamePassword(credentialsId: DOCKER_REGISTRY_CREDENTIAL,
+                usernameVariable: 'DOCKER_REGISTRY_USER', passwordVariable: 'DOCKER_REGISTRY_PASSWORD')]) {
+              sh 'docker login -u=${DOCKER_REGISTRY_USER} -p=${DOCKER_REGISTRY_PASSWORD} ${DOCKER_REGISTRY_SERVER}'
+              sh 'docker image push ${DOCKER_REGISTRY_USER}/${ARTIFACT_ID}:${ARTIFACT_VERSION}'
+              sh 'docker image push ${DOCKER_REGISTRY_USER}/${ARTIFACT_ID}:${gitCommitId}'
+              if ('master' == ${env.BRANCH_NAME}) {
+                sh 'docker image push ${DOCKER_REGISTRY_USER}/${ARTIFACT_ID}:latest'
+              }
+              sh 'docker logout'
+            }
+            //withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: DOCKER_REGISTRY_CREDENTIAL,
+            //                  usernameVariable: 'DOCKER_REGISTRY_USER', passwordVariable: 'DOCKER_REGISTRY_PASSWORD']]) {
+            //}
+          }
         }
       }
 
