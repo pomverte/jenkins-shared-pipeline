f831bcef6906b4ba14169c879e2b0267c929d2ce
pomverte <vietnem@yahoo.fr>
2018-08-09 00:38:40 +0200

    git format message

--- vars/displayInformation.groovy
+++ vars/displayInformation.groovy
@@ -1,9 +1,9 @@
 def call() {
 
   gitCommitId = sh(returnStdout: true, script: 'git rev-parse --short HEAD').trim()
-  def author = sh(returnStdout: true, script: 'git --no-pager show -s --format=\'%an\' HEAD').trim()
-  def authorEmail = sh(returnStdout: true, script: 'git --no-pager show -s --format=\'%ae\' HEAD').trim()
-  def comment = sh(returnStdout: true, script: 'git --no-pager show -s --format=\'%B\' HEAD').trim()
+  def author = sh(returnStdout: true, script: 'git --no-pager show -s --format=%an HEAD').trim()
+  def authorEmail = sh(returnStdout: true, script: 'git --no-pager show -s --format=%ae HEAD').trim()
+  def comment = sh(returnStdout: true, script: 'git --no-pager show -s --format=%B HEAD').trim()
 
   echo """
 
