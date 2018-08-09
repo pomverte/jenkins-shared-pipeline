731cb6af0d0a3f8bc3de820cb5970aec6c0eecaa
hvle <ha.hong.viet.le@cgi.com>
2018-08-09 11:13:43 +0200

    git fix format

--- vars/notifySlack.groovy
+++ vars/notifySlack.groovy
@@ -9,7 +9,7 @@ def call(String buildStatus, String[] channels = []) {
       def jobNameDecoded = URLDecoder.decode("${env.JOB_NAME}", "UTF-8")
       def jobNameSplitted = jobNameDecoded.split('/')
       def jobNameShort = jobNameSplitted.size() > 1 ? jobNameSplitted[1] : jobNameDecoded
-      def author = sh(returnStdout: true, script: 'git --no-pager show -s --format=\'%an\' HEAD').trim()
+      def author = sh(returnStdout: true, script: 'git --no-pager show -s --format=%an HEAD').trim()
 
       channels << '#jenkins'
       channels.each {
