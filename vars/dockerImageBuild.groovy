c4e1e56112f9052653682d0134fc53a72b72d201
hvle <ha.hong.viet.le@cgi.com>
2018-08-10 10:32:40 +0200

    git commitId is a label not a tag

--- vars/dockerImageBuild.groovy
+++ vars/dockerImageBuild.groovy
@@ -1,6 +1,6 @@
 def call() {
   def sha1 = gitCommitId()
-  sh "docker image build -t ${DOCKER_REGISTRY_USER}/${ARTIFACT_ID}:${ARTIFACT_VERSION} -t ${DOCKER_REGISTRY_USER}/${ARTIFACT_ID}:${sha1} ."
+  sh "docker image build --label 'git.commit.id=${sha1}' -t ${DOCKER_REGISTRY_USER}/${ARTIFACT_ID}:${ARTIFACT_VERSION} ."
   if ('master' == "${env.BRANCH_NAME}") {
     sh "docker image tag ${DOCKER_REGISTRY_USER}/${ARTIFACT_ID}:${ARTIFACT_VERSION} ${DOCKER_REGISTRY_USER}/${ARTIFACT_ID}:latest"
   }
