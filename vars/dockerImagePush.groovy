4ef128e2b8f35b5623687b022c937e9c311d611c
pomverte <vietnem@yahoo.fr>
2018-08-10 23:14:45 +0200

    remove tag with git commit id

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -3,7 +3,6 @@ def call() {
 //    sh "docker login -u=${DOCKER_REGISTRY_USER} -p=${DOCKER_REGISTRY_PASSWORD} ${DOCKER_REGISTRY_SERVER}"
     sh "docker login -u=${DOCKER_REGISTRY_USER} -p=${DOCKER_REGISTRY_PASSWORD}"
     sh "docker image push ${DOCKER_REGISTRY_USER}/${ARTIFACT_ID}:${ARTIFACT_VERSION}"
-    sh "docker image push ${DOCKER_REGISTRY_USER}/${ARTIFACT_ID}:" + gitCommitId()
     if ('master' == "${env.BRANCH_NAME}") {
       sh "docker image push ${DOCKER_REGISTRY_USER}/${ARTIFACT_ID}:latest"
     }
