a37c58974b115875c863225a8fabc3eddda9e46d
hvle <ha.hong.viet.le@cgi.com>
2018-08-10 10:45:53 +0200

    docker label with commit author

--- vars/dockerImageBuild.groovy
+++ vars/dockerImageBuild.groovy
@@ -1,6 +1,7 @@
 def call() {
+  def author = sh(returnStdout: true, script: 'git --no-pager show -s --format=\'%an <%ae>\' HEAD').trim()
   def sha1 = gitCommitId()
-  sh "docker image build --label 'git.commit.id=${sha1}' -t ${DOCKER_REGISTRY_USER}/${ARTIFACT_ID}:${ARTIFACT_VERSION} ."
+  sh "docker image build --label 'maintainer=${author}' --label 'git.commit.id=${sha1}' -t ${DOCKER_REGISTRY_USER}/${ARTIFACT_ID}:${ARTIFACT_VERSION} ."
   if ('master' == "${env.BRANCH_NAME}") {
     sh "docker image tag ${DOCKER_REGISTRY_USER}/${ARTIFACT_ID}:${ARTIFACT_VERSION} ${DOCKER_REGISTRY_USER}/${ARTIFACT_ID}:latest"
   }
