47a5ab657b9c09972c05312515d7ba4a1d6dbf23
hvle <ha.hong.viet.le@cgi.com>
2018-08-13 11:53:21 +0200

    feat: hadolint

--- vars/dockerPipeline.groovy
+++ vars/dockerPipeline.groovy
@@ -35,14 +35,29 @@ def call(Closure body) {
         }
       }
 
-      stage('Docker image build and tag') {
+      stage('Linting') {
+        environment {
+          HADOLINT_DOCKER_IMAGE = 'hadolint/hadolint:v1.10.4'
+          HADOLINT_DOCKER_CMD = 'hadolint --ignore DL3018 --ignore DL3013 /tmp/Dockerfile'
+          JENKINS_VOLUME = 'gitea_jenkins_jenkins_home' // FIXME hard coded docker volume name here ...
+        }
+        steps {
+          script {
+            def path = pwd().split('/')
+            def workspace = "/var/lib/docker/volumes/${JENKINS_VOLUME}/_data/workspace/" + path[path.size() - 1]
+            sh "docker container run --rm -i -v ${workspace}/Dockerfile:/tmp/Dockerfile ${HADOLINT_DOCKER_IMAGE} ${HADOLINT_DOCKER_CMD}"
+          }
+        }
+      }
+
+      stage('Image build and tag') {
         steps {
           script {
             dockerImage.build()
           }
         }
       }
-      stage('Docker image push') {
+      stage('Image push') {
         when {
           environment name: 'PUSH_DOCKER_IMAGE', value: 'true'
         }
