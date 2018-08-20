72eca51b1560d847701dc9a6816845aedc754b7a
hvle <ha.hong.viet.le@cgi.com>
2018-08-20 14:50:17 +0200

    maven add settings

--- vars/dockerImage.groovy
+++ vars/dockerImage.groovy
@@ -6,7 +6,10 @@
 def build() {
   def author = gitme.commitAuthor()
   def sha1 = gitme.commitId()
-  sh "docker image build --label 'maintainer=${author}' --label 'git.commit.id=${sha1}' -t ${DOCKER_REGISTRY_USER}/${ARTIFACT_ID}:${ARTIFACT_VERSION} ."
+  configFileProvider([configFile(fileId: 'maven-settings', variable: 'MAVEN_SETTINGS')]) {
+    sh "cp ${MAVEN_SETTINGS} settings.xml"
+    sh "docker image build --label 'maintainer=${author}' --label 'git.commit.id=${sha1}' -t ${DOCKER_REGISTRY_USER}/${ARTIFACT_ID}:${ARTIFACT_VERSION} ."
+  }
   if ('master' == "${env.BRANCH_NAME}") {
     sh "docker image tag ${DOCKER_REGISTRY_USER}/${ARTIFACT_ID}:${ARTIFACT_VERSION} ${DOCKER_REGISTRY_USER}/${ARTIFACT_ID}:latest"
   }
